b768ad8e3839fa89a1571d0840ecbf4c04d55374
Samuel Holland <samuel.holland@sifive.com>
2026-02-04 19:19:27 +0000

    kasan: arm64: x86: Make special tags arch specific
    KASAN's tag-based mode defines multiple special tag values. They're
    reserved for:
    - Native kernel value. On arm64 it's 0xFF and it causes an early return
      in the tag checking function.
    - Invalid value. 0xFE marks an area as freed / unallocated. It's also
      the value that is used to initialize regions of shadow memory.
    - Min and max values. 0xFD is the highest value that can be randomly
      generated for a new tag. 0 is the minimal value with the exception of
      arm64's hardware mode where it is equal to 0xF0.
    
    Metadata macro is also defined:
    - Tag width equal to 8.
    
    Tag-based mode on x86 is going to use 4 bit wide tags so all the above
    values need to be changed accordingly.
    
    Make tag width and native kernel tag arch specific for x86 and arm64.
    
    Base the invalid tag value and the max value on the native kernel tag
    since they follow the same pattern on both mentioned architectures.
    
    Also generalize KASAN_SHADOW_INIT and 0xff used in various
    page_kasan_tag* helpers.
    
    Give KASAN_TAG_MIN the default value of zero, and move the special value
    for hw_tags arm64 to its arch specific kasan-tags.h.
    
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Co-developed-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
    Acked-by: Will Deacon <will@kernel.org> (for the arm part)
    Reviewed-by: Andrey Konovalov <andreyknvl@gmail.com>
    Reviewed-by: Andrey Ryabinin <ryabinin.a.a@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2165,7 +2165,7 @@ static inline u8 page_kasan_tag(const struct page *page)
 
 	if (kasan_enabled()) {
 		tag = (page->flags.f >> KASAN_TAG_PGSHIFT) & KASAN_TAG_MASK;
-		tag ^= 0xff;
+		tag ^= KASAN_TAG_KERNEL;
 	}
 
 	return tag;
@@ -2178,7 +2178,7 @@ static inline void page_kasan_tag_set(struct page *page, u8 tag)
 	if (!kasan_enabled())
 		return;
 
-	tag ^= 0xff;
+	tag ^= KASAN_TAG_KERNEL;
 	old_flags = READ_ONCE(page->flags.f);
 	do {
 		flags = old_flags;
@@ -2197,7 +2197,7 @@ static inline void page_kasan_tag_reset(struct page *page)
 
 static inline u8 page_kasan_tag(const struct page *page)
 {
-	return 0xff;
+	return KASAN_TAG_KERNEL;
 }
 
 static inline void page_kasan_tag_set(struct page *page, u8 tag) { }
