2cffa7070b7bb0ff894f12d8815fe2607fb02530
Brendan Jackman <jackmanb@google.com>
2026-02-25 16:34:31 +0000

    mm: introduce for_each_free_list()
    Later patches will rearrange the free areas, but there are a couple of
    places that iterate over them with the assumption that they have the
    current structure.
    
    It seems ideally, code outside of mm should not be directly aware of
    struct free_area in the first place, but that awareness seems relatively
    harmless so just make the minimal change.
    
    Now instead of letting users manually iterate over the free lists, just
    provide a macro to do that. Then adopt that macro in a couple of places.
    
    Signed-off-by: Brendan Jackman <jackmanb@google.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -123,9 +123,12 @@ static inline bool migratetype_is_mergeable(int mt)
 	return mt < MIGRATE_PCPTYPES;
 }
 
-#define for_each_migratetype_order(order, type) \
+#define for_each_free_list(list, zone, order) \
 	for (order = 0; order < NR_PAGE_ORDERS; order++) \
-		for (type = 0; type < MIGRATE_TYPES; type++)
+		for (unsigned int type = 0; \
+		     list = &zone->free_area[order].free_list[type], \
+		     type < MIGRATE_TYPES; \
+		     type++) \
 
 extern int page_group_by_mobility_disabled;
 
