1900a7c0b9d45f63e7969ab277ccc15685fd27d8
Jeff Layton <jlayton@kernel.org>
2026-02-26 10:55:53 -0500

    security: update audit format strings for u64 i_ino
    Update %lu/%ld to %llu/%lld in security audit logging functions that
    print inode->i_ino, since i_ino is now u64.
    
    Files updated: apparmor/apparmorfs.c, integrity/integrity_audit.c,
    ipe/audit.c, lsm_audit.c.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-By: Ryan Lee <ryan.lee@canonical.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1400,7 +1400,7 @@ static int inode_doinit_use_xattr(struct inode *inode, struct dentry *dentry,
 	if (rc < 0) {
 		kfree(context);
 		if (rc != -ENODATA) {
-			pr_warn("SELinux: %s:  getxattr returned %d for dev=%s ino=%ld\n",
+			pr_warn("SELinux: %s:  getxattr returned %d for dev=%s ino=%lld\n",
 				__func__, -rc, inode->i_sb->s_id, inode->i_ino);
 			return rc;
 		}
@@ -3477,7 +3477,7 @@ static void selinux_inode_post_setxattr(struct dentry *dentry, const char *name,
 					   &newsid);
 	if (rc) {
 		pr_err("SELinux:  unable to map context to SID"
-		       "for (%s, %lu), rc=%d\n",
+		       "for (%s, %llu), rc=%d\n",
 		       inode->i_sb->s_id, inode->i_ino, -rc);
 		return;
 	}
