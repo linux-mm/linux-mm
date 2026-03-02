7660820e349dddde8688cc74162dca48f9dbaf33
Jeff Layton <jlayton@kernel.org>
2026-03-02 15:25:25 -0500

    fscrypt: replace PRIino with %llu/%llx format strings
    Now that i_ino is u64 and the PRIino format macro has been removed,
    replace all uses in fscrypt with the concrete format strings.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/crypto/keyring.c
+++ fs/crypto/keyring.c
@@ -970,7 +970,7 @@ static int check_for_busy_inodes(struct super_block *sb,
 	struct list_head *pos;
 	size_t busy_count = 0;
 	char ino_str[50] = "";
-	kino_t ino;
+	u64 ino;
 
 	spin_lock(&mk->mk_decrypted_inodes_lock);
 
@@ -994,7 +994,7 @@ static int check_for_busy_inodes(struct super_block *sb,
 
 	/* If the inode is currently being created, ino may still be 0. */
 	if (ino)
-		snprintf(ino_str, sizeof(ino_str), ", including ino %" PRIino "u", ino);
+		snprintf(ino_str, sizeof(ino_str), ", including ino %llu", ino);
 
 	fscrypt_warn(NULL,
 		     "%s: %zu inode(s) still busy after removing key with %s %*phN%s",
