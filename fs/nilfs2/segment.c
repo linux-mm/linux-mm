75eaeca693df2ca6f70d56a586fff68e9e70761f
Jeff Layton <jlayton@kernel.org>
2026-03-02 15:24:52 -0500

    nilfs2: replace PRIino with %llu/%llx format strings
    Now that i_ino is u64 and the PRIino format macro has been removed,
    replace all uses in nilfs2 with the concrete format strings.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Viacheslav Dubeyko <slava@dubeyko.com>

--- fs/nilfs2/segment.c
+++ fs/nilfs2/segment.c
@@ -2024,7 +2024,7 @@ static int nilfs_segctor_collect_dirty_files(struct nilfs_sc_info *sci,
 				ifile, ii->vfs_inode.i_ino, &ibh);
 			if (unlikely(err)) {
 				nilfs_warn(sci->sc_super,
-					   "log writer: error %d getting inode block (ino=%" PRIino "u)",
+					   "log writer: error %d getting inode block (ino=%llu)",
 					   err, ii->vfs_inode.i_ino);
 				return err;
 			}
