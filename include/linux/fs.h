2fa06ec23ab8ebc64128ac103160c240ffcda4d1
Jan Kara <jack@suse.cz>
2026-03-03 11:34:21 +0100

    fs: Drop i_private_list from address_space
    Nobody is using i_private_list anymore. Remove it.
    
    Signed-off-by: Jan Kara <jack@suse.cz>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -470,7 +470,6 @@ struct mapping_metadata_bhs {
  * @flags: Error bits and flags (AS_*).
  * @wb_err: The most recent error which has occurred.
  * @i_private_lock: For use by the owner of the address_space.
- * @i_private_list: For use by the owner of the address_space.
  */
 struct address_space {
 	struct inode		*host;
@@ -489,7 +488,6 @@ struct address_space {
 	unsigned long		flags;
 	errseq_t		wb_err;
 	spinlock_t		i_private_lock;
-	struct list_head	i_private_list;
 	struct rw_semaphore	i_mmap_rwsem;
 } __attribute__((aligned(sizeof(long)))) __randomize_layout;
 	/*
