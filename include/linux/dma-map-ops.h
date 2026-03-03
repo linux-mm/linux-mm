ca80f0dd1bcce07f224826aa6e4b0b0a796048ae
Maxime Ripard <mripard@kernel.org>
2026-03-03 11:13:05 +0100

    dma: contiguous: Make dev_get_cma_area() a proper function
    As we try to enable dma-buf heaps, and the CMA one in particular, to
    compile as modules, we need to export dev_get_cma_area(). It's currently
    implemented as an inline function that returns either the content of
    device->cma_area or dma_contiguous_default_area.
    
    Thus, it means we need to export dma_contiguous_default_area, which
    isn't really something we want any module to have access to.
    
    Instead, let's make dev_get_cma_area() a proper function we will be able
    to export so we can avoid exporting dma_contiguous_default_area.
    
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- include/linux/dma-map-ops.h
+++ include/linux/dma-map-ops.h
@@ -93,12 +93,7 @@ static inline void set_dma_ops(struct device *dev,
 #ifdef CONFIG_DMA_CMA
 extern struct cma *dma_contiguous_default_area;
 
-static inline struct cma *dev_get_cma_area(struct device *dev)
-{
-	if (dev && dev->cma_area)
-		return dev->cma_area;
-	return dma_contiguous_default_area;
-}
+struct cma *dev_get_cma_area(struct device *dev);
 
 void dma_contiguous_reserve(phys_addr_t addr_limit);
 int __init dma_contiguous_reserve_area(phys_addr_t size, phys_addr_t base,
@@ -112,7 +107,6 @@ struct page *dma_alloc_contiguous(struct device *dev, size_t size, gfp_t gfp);
 void dma_free_contiguous(struct device *dev, struct page *page, size_t size);
 
 void dma_contiguous_early_fixup(phys_addr_t base, unsigned long size);
-struct cma *dma_contiguous_get_reserved_region(unsigned int idx);
 #else /* CONFIG_DMA_CMA */
 static inline struct cma *dev_get_cma_area(struct device *dev)
 {
@@ -151,11 +145,16 @@ static inline void dma_free_contiguous(struct device *dev, struct page *page,
 static inline void dma_contiguous_early_fixup(phys_addr_t base, unsigned long size)
 {
 }
+#endif /* CONFIG_DMA_CMA*/
+
+#if defined(CONFIG_DMA_CMA) && defined(CONFIG_OF_RESERVED_MEM)
+struct cma *dma_contiguous_get_reserved_region(unsigned int idx);
+#else
 static inline struct cma *dma_contiguous_get_reserved_region(unsigned int idx)
 {
 	return NULL;
 }
-#endif /* CONFIG_DMA_CMA*/
+#endif
 
 #ifdef CONFIG_DMA_DECLARE_COHERENT
 int dma_declare_coherent_memory(struct device *dev, phys_addr_t phys_addr,
