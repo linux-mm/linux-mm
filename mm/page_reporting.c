0dc62a7d7443e50db9cfc9907da2da1c8e530f89
Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>
2026-03-03 01:33:37 -0800

    mm/page_reporting: add PAGE_REPORTING_ORDER_UNSPECIFIED
    Drivers can pass order of pages to be reported while
    registering itself. Today, this is a magic number, 0.
    
    Label this with PAGE_REPORTING_ORDER_UNSPECIFIED and
    check for it when the driver is being registered.
    
    This macro will be used in relevant drivers next.
    
    Signed-off-by: Yuvraj Sakshith <yuvraj.sakshith@oss.qualcomm.com>

--- mm/page_reporting.c
+++ mm/page_reporting.c
@@ -369,8 +369,9 @@ int page_reporting_register(struct page_reporting_dev_info *prdev)
 	 * pageblock_order.
 	 */
 
-	if (page_reporting_order == -1) {
-		if (prdev->order > 0 && prdev->order <= MAX_PAGE_ORDER)
+	if (page_reporting_order == PAGE_REPORTING_ORDER_UNSPECIFIED) {
+		if (prdev->order != PAGE_REPORTING_ORDER_UNSPECIFIED &&
+			prdev->order <= MAX_PAGE_ORDER)
 			page_reporting_order = prdev->order;
 		else
 			page_reporting_order = pageblock_order;
