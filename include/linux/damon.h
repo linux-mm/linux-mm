1484decb5c2d95e930463ea919650401b5fd75af
SeongJae Park <sj@kernel.org>
2026-03-03 20:41:10 -0800

    mm/damon/core: introduce damos_quota_goal_tuner
    Currently DAMOS quota goal feature utilizes a single feedback loop based
    algorithm for automatic tuning of the effective quota.  It is useful in
    dynamic environments that operate systems with only kernels in the long
    term.  But, no one fits all.  It is not very easy to control in
    environments having more controlled characteristics and user-space
    control towers.  We actually got multiple reports [1,2] of use cases
    that the algorithm is not optimal.
    
    Introduce a new field of 'struct damos_quotas', namely 'goal_tuner'.  It
    specifies what tuning algorithm the given scheme should use, and allows
    DAMON API callers to set it as they want.  Nonetheless, this commit
    introduces no new tuning algorithm but only the interface.  This commit
    hence makes no behavioral change.  A new algorithm will be added by the
    following commit.
    
    [1] https://lore.kernel.org/CALa+Y17__d=ZsM1yX+MXx0ozVdsXnFqF4p0g+kATEitrWyZFfg@mail.gmail.com
    [2] https://lore.kernel.org/20260204022537.814-1-yunjeong.mun@sk.com
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -216,6 +216,14 @@ struct damos_quota_goal {
 };
 
 /**
+ * enum damos_quota_goal_tuner - Goal-based quota tuning logic.
+ * @DAMOS_QUOTA_GOAL_TUNER_CONSIST:	Aim long term consistent quota.
+ */
+enum damos_quota_goal_tuner {
+	DAMOS_QUOTA_GOAL_TUNER_CONSIST,
+};
+
+/**
  * struct damos_quota - Controls the aggressiveness of the given scheme.
  * @reset_interval:	Charge reset interval in milliseconds.
  * @ms:			Maximum milliseconds that the scheme can use.
@@ -262,6 +270,7 @@ struct damos_quota {
 	unsigned long ms;
 	unsigned long sz;
 	struct list_head goals;
+	enum damos_quota_goal_tuner goal_tuner;
 	unsigned long esz;
 
 	unsigned int weight_sz;
