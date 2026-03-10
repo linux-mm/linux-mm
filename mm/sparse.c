569144a408098e616acf91e681dfae2a9b4931d0
Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
2026-03-11 01:46:03 +0530

    mm/sparse: Register information into meminspect
    Annotate vital static, dynamic information into meminspect for
    debugging:
     - mem_section (static)
     - section (dynamic)
     - mem_section_usage(dynamic)
    
    meminspect uses a different API to annotate variables for inspection,
    and information about these variables is stored in the inspection table.
    
    Co-developed-by: Eugen Hristev <eugen.hristev@linaro.org>
    Signed-off-by: Eugen Hristev <eugen.hristev@linaro.org>
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>

--- mm/sparse.c
+++ mm/sparse.c
@@ -15,6 +15,7 @@
 #include <linux/swapops.h>
 #include <linux/bootmem_info.h>
 #include <linux/vmstat.h>
+#include <linux/meminspect.h>
 #include "internal.h"
 #include <asm/dma.h>
 
@@ -30,6 +31,7 @@ struct mem_section mem_section[NR_SECTION_ROOTS][SECTIONS_PER_ROOT]
 	____cacheline_internodealigned_in_smp;
 #endif
 EXPORT_SYMBOL(mem_section);
+MEMINSPECT_SIMPLE_ENTRY(mem_section);
 
 #ifdef NODE_NOT_IN_PAGE_FLAGS
 /*
@@ -253,6 +255,7 @@ static void __init memblocks_present(void)
 		size = sizeof(struct mem_section *) * NR_SECTION_ROOTS;
 		align = 1 << (INTERNODE_CACHE_SHIFT);
 		mem_section = memblock_alloc_or_panic(size, align);
+		meminspect_register_va(mem_section, size);
 	}
 #endif
 
@@ -343,6 +346,9 @@ again:
 		limit = MEMBLOCK_ALLOC_ACCESSIBLE;
 		goto again;
 	}
+
+	meminspect_register_va(usage, size);
+
 	return usage;
 }
 
