3eca4a8ecb26295d053d3ec8dd5b9e1aab950413
Asier Gutierrez <gutierrez.asier@huawei-partners.com>
2026-03-10 16:24:17 +0000

    mm/damon: Generic context creation for modules
    It is more elegant to have a generic version of new context creation
    which receives the mode as a parameter.
    
    Signed-off-by: Asier Gutierrez <gutierrez.asier@huawei-partners.com>
    Co-developed-by: Anatoly Stepanov <stepanov.anatoly@huawei.com>

--- mm/damon/modules-common.c
+++ mm/damon/modules-common.c
@@ -14,8 +14,8 @@
  * @ctxp:	Pointer to save the point to the newly created context
  * @targetp:	Pointer to save the point to the newly created target
  */
-int damon_modules_new_paddr_ctx_target(struct damon_ctx **ctxp,
-		struct damon_target **targetp)
+int damon_modules_new_ctx_target(struct damon_ctx **ctxp,
+		struct damon_target **targetp, enum damon_ops_id mode)
 {
 	struct damon_ctx *ctx;
 	struct damon_target *target;
@@ -24,7 +24,7 @@ int damon_modules_new_paddr_ctx_target(struct damon_ctx **ctxp,
 	if (!ctx)
 		return -ENOMEM;
 
-	if (damon_select_ops(ctx, DAMON_OPS_PADDR)) {
+	if (damon_select_ops(ctx, mode)) {
 		damon_destroy_ctx(ctx);
 		return -EINVAL;
 	}
