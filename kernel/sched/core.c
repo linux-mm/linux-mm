9a1f005ad081ede27a0957b8d7748776bab8d6eb
Eugen Hristev <eugen.hristev@linaro.org>
2026-03-11 01:46:01 +0530

    sched/core: Annotate runqueues into meminspect
    Annotate runqueues into meminspect. Even if these are static, they are
    defined percpu, and a later init call will instantiate them for each
    cpu. Hence, we cannot annotate them in the usual way, but rather have to
    call meminspect API at init time.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@linaro.org>
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -69,6 +69,7 @@
 #include <linux/wait_api.h>
 #include <linux/workqueue_api.h>
 #include <linux/livepatch_sched.h>
+#include <linux/meminspect.h>
 
 #ifdef CONFIG_PREEMPT_DYNAMIC
 # ifdef CONFIG_GENERIC_IRQ_ENTRY
@@ -8750,6 +8751,7 @@ void __init sched_init(void)
 		rq->core_cookie = 0UL;
 #endif
 		zalloc_cpumask_var_node(&rq->scratch_mask, GFP_KERNEL, cpu_to_node(i));
+		meminspect_lock_register_va(rq, sizeof(*rq));
 	}
 
 	set_load_weight(&init_task, false);
