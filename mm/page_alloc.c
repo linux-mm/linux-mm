4318797e2ed1a033a1ab8df19cc8426b78a7e8bc
Dmitry Ilvokhin <d@ilvokhin.com>
2026-04-08 13:33:22 +0000

    mm: use zone lock guard in free_pcppages_bulk()
    Use spinlock_irqsave zone lock guard in free_pcppages_bulk() to replace
    the explicit lock/unlock pattern with automatic scope-based cleanup.
    
    Suggested-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Dmitry Ilvokhin <d@ilvokhin.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1451,7 +1451,6 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 					struct per_cpu_pages *pcp,
 					int pindex)
 {
-	unsigned long flags;
 	unsigned int order;
 	struct page *page;
 
@@ -1464,7 +1463,7 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 	/* Ensure requested pindex is drained first. */
 	pindex = pindex - 1;
 
-	spin_lock_irqsave(&zone->lock, flags);
+	guard(spinlock_irqsave)(&zone->lock);
 
 	while (count > 0) {
 		struct list_head *list;
@@ -1496,8 +1495,6 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 			trace_mm_page_pcpu_drain(page, order, mt);
 		} while (count > 0 && !list_empty(list));
 	}
-
-	spin_unlock_irqrestore(&zone->lock, flags);
 }
 
 /* Split a multi-block free page into its individual pageblocks. */
