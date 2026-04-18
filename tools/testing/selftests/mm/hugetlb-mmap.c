d7a91fd2e4310e6d80f3930558cbda1b972f3915
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-04-18 13:55:23 +0300

    selftests/mm: hugetlb-mmap: add setup of HugeTLB pages
    hugetlb-mmap test fails if there are no free huge pages prepared by a
    wrapper script.
    
    Add setup of HugeTLB pages to the test and make sure that the original
    settings are restored on the test exit.
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- tools/testing/selftests/mm/hugetlb-mmap.c
+++ tools/testing/selftests/mm/hugetlb-mmap.c
@@ -106,10 +106,9 @@ int main(int argc, char **argv)
 {
 	size_t hugepage_size;
 	size_t length = LENGTH;
-	int shift = 0;
+	int shift = 0, nr;
 
 	ksft_print_header();
-	ksft_set_plan(2);
 
 	if (argc > 1)
 		length = atol(argv[1]) << 20;
@@ -125,9 +124,13 @@ int main(int argc, char **argv)
 	}
 
 	/* munmap with fail if the length is not page aligned */
-	if (hugepage_size > length)
-		length = hugepage_size;
+	length = (length + hugepage_size) & ~(hugepage_size - 1);
+	nr = length / hugepage_size;
 
+	if (!hugetlb_setup_default(nr))
+		ksft_exit_skip("Not enough %lu Kb pages\n", hugepage_size >> 10);
+
+	ksft_set_plan(2);
 	ksft_print_msg("Mapping %lu Mbytes\n", (unsigned long)length >> 20);
 
 	test_anon_mmap(length, shift);
