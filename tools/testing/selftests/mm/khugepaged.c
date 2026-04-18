64ff365bb8a9bbf352b1a0c8af071dac0acbf4c2
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-04-18 13:54:57 +0300

    selftests/mm: khugepaged: group tests in an array
    Currently khugepaged decides if a test can run using TEST() macro that
    checks what mem_ops and collapse_context are set by the command line
    arguments.
    
    For better compatibility with ksefltest framework, add an array of 'struct
    test_case's and redefine TEST() macro to conditionally add enabled tests to
    that array.
    
    Then execute the enabled test by looping the test_case's array.
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- tools/testing/selftests/mm/khugepaged.c
+++ tools/testing/selftests/mm/khugepaged.c
@@ -1161,6 +1161,34 @@ static void parse_test_type(int argc, char **argv)
 	get_finfo(argv[1]);
 }
 
+typedef void (*test_fn)(struct collapse_context *c, struct mem_ops *ops);
+
+struct test_case {
+	struct collapse_context *ctx;
+	struct mem_ops *ops;
+	const char *desc;
+	test_fn fn;
+};
+
+#define MAX_TEST_CASES 64
+static struct test_case test_cases[MAX_TEST_CASES];
+static int nr_test_cases;
+
+#define TEST(t, c, o) do {						\
+	if (c && o) {							\
+		if (nr_test_cases >= MAX_TEST_CASES) {			\
+			printf("MAX_ADD_CASES is too small\n");		\
+			exit(EXIT_FAILURE);				\
+		}							\
+		test_cases[nr_test_cases++] = (struct test_case){	\
+			.ctx	= c,					\
+			.ops	= o,					\
+			.desc	= #t,					\
+			.fn	= t,					\
+		};							\
+	}								\
+	} while (0)
+
 int main(int argc, char **argv)
 {
 	int hpage_pmd_order;
@@ -1215,13 +1243,6 @@ int main(int argc, char **argv)
 	thp_push_settings(&default_settings);
 
 	alloc_at_fault();
-
-#define TEST(t, c, o) do { \
-	if (c && o) { \
-		printf("\nRun test: " #t " (%s:%s)\n", c->name, o->name); \
-		t(c, o); \
-	} \
-	} while (0)
 
 	TEST(collapse_full, khugepaged_context, anon_ops);
 	TEST(collapse_full, khugepaged_context, file_ops);
@@ -1283,6 +1304,14 @@ int main(int argc, char **argv)
 
 	TEST(madvise_retracted_page_tables, madvise_context, file_ops);
 	TEST(madvise_retracted_page_tables, madvise_context, shmem_ops);
+
+	for (int i = 0; i < nr_test_cases; i++) {
+		struct test_case *t = &test_cases[i];
+
+		exit_status = KSFT_PASS;
+		printf("\nRun test: %s: (%s:%s)\n", t->desc, t->ctx->name, t->ops->name);
+		t->fn(t->ctx, t->ops);
+	}
 
 	restore_settings(0);
 }
