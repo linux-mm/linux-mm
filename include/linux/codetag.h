83fb0cc3e54d2186617c99b42cda5fe5baeba4c3
Suren Baghdasaryan <surenb@google.com>
2026-05-04 23:36:19 +0000

    alloc_tag: add ioctl to /proc/allocinfo
    Add the following ioctl commands for /proc/allocinfo file:
    
    ALLOCINFO_IOC_CONTENT_ID - gets content identifier which can be used
    to check whether the file content has changed specifically due to module
    load/unload. Every time a module is loaded / unloaded, the returned
    value will be different. By comparing the identifier value at the
    beginning and at the end of the content retrieval operation, users can
    validate retrieved information for consistency.
    
    ALLOCINFO_IOC_GET_AT - gets the record at the specified position. This
    is the position of a record in /proc/allocinfo.
    
    ALLOCINFO_IOC_GET_NEXT - gets the record next to the last retrieved
    one. If no records were previously retrieved, returns the first
    record.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Abhishek Bapat <abhishekbapat@google.com>

--- include/linux/codetag.h
+++ include/linux/codetag.h
@@ -76,6 +76,7 @@ struct codetag_iterator {
 
 void codetag_lock_module_list(struct codetag_type *cttype, bool lock);
 bool codetag_trylock_module_list(struct codetag_type *cttype);
+unsigned long codetag_get_content_id(struct codetag_type *cttype);
 struct codetag_iterator codetag_get_ct_iter(struct codetag_type *cttype);
 struct codetag *codetag_next_ct(struct codetag_iterator *iter);
 
