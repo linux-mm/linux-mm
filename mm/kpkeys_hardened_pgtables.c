820fe82c0fbf6b3a2e246fde7cd8d17f8bc48206
Kevin Brodsky <kevin.brodsky@arm.com>
2026-05-05 17:06:01 +0100

    mm: kpkeys: Protect regular page tables
    If the kpkeys_hardened_pgtables feature is enabled, page table pages
    (PTPs) should be protected by modifying the linear mapping to map
    them with a privileged pkey (KPKEYS_PKEY_PGTABLES). This patch
    introduces a new page allocator for that purpose:
    
    * kpkeys_pgtable_alloc() allocates a new PTP and sets the linear
      mapping to KPKEYS_PKEY_PGTABLES for that page
    
    * kpkeys_pgtable_free() frees such a PTP and restores the linear
      mapping to the default pkey
    
    This interface is then hooked into pagetable_alloc() and
    pagetable_free(), protecting all page tables created once the buddy
    allocator is available. Early page tables are allocated in other
    ways and will be protected in subsequent patches.
    
    This implementation of kpkeys_pgtable_{alloc,free}() is minimal and
    relies on the linear map being fully PTE-mapped - otherwise
    calling set_memory_pkey() on a single page may result in splitting a
    block mapping, which in turn requires allocating a new PTP. A more
    elaborate implementation could be added later to handle this
    situation.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- mm/kpkeys_hardened_pgtables.c
+++ mm/kpkeys_hardened_pgtables.c
@@ -1,11 +1,58 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include <linux/kpkeys.h>
 #include <linux/mm.h>
+#include <linux/set_memory.h>
 
 #include <kunit/visibility.h>
 
 __ro_after_init DEFINE_STATIC_KEY_FALSE(kpkeys_hardened_pgtables_key);
 EXPORT_SYMBOL_IF_KUNIT(kpkeys_hardened_pgtables_key);
+
+static int set_pkey_pgtable(struct page *page, unsigned int nr_pages)
+{
+	unsigned long addr = (unsigned long)page_address(page);
+	int ret;
+
+	ret = set_memory_pkey(addr, nr_pages, KPKEYS_PKEY_PGTABLES);
+
+	WARN_ON(ret);
+	return ret;
+}
+
+static int set_pkey_default(struct page *page, unsigned int nr_pages)
+{
+	unsigned long addr = (unsigned long)page_address(page);
+	int ret;
+
+	ret = set_memory_pkey(addr, nr_pages, KPKEYS_PKEY_DEFAULT);
+
+	WARN_ON(ret);
+	return ret;
+}
+
+struct page *kpkeys_pgtable_alloc(gfp_t gfp, unsigned int order)
+{
+	struct page *page;
+	int ret;
+
+	page = alloc_pages_noprof(gfp, order);
+	if (!page)
+		return page;
+
+	ret = set_pkey_pgtable(page, 1 << order);
+	if (ret) {
+		__free_pages(page, order);
+		return NULL;
+	}
+
+	return page;
+}
+
+void kpkeys_pgtable_free(struct page *page, unsigned int order)
+{
+	set_pkey_default(page, 1 << order);
+	__free_pages(page, order);
+}
 
 void __init kpkeys_hardened_pgtables_init(void)
 {
