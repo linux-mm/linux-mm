8bbb4f82fdb2616d5c604f400e2dca92dbf0a8af
Barry Song (Xiaomi) <baohua@kernel.org>
2026-05-14 17:41:08 +0800

    mm/vmalloc: Stop scanning for compound pages after encountering small pages in vmap
    Users typically allocate memory in descending orders, e.g.
    8 → 4 → 0. Once an order-0 page is encountered, subsequent
    pages are likely to also be order-0, so we stop scanning
    for compound pages at that point.
    
    Signed-off-by: Barry Song (Xiaomi) <baohua@kernel.org>
    Signed-off-by: Wen Jiang <jiangwen6@xiaomi.com>
    Tested-by: Xueyuan Chen <xueyuan.chen21@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -3596,6 +3596,12 @@ static int __vmap_huge(unsigned long addr, unsigned long end,
 			map_addr = addr;
 			idx = i;
 		}
+		/*
+		 * Once small pages are encountered, the remaining pages
+		 * are likely small as well
+		 */
+		if (shift == PAGE_SHIFT)
+			break;
 
 		addr += 1UL << shift;
 		i += 1U << (shift - PAGE_SHIFT);
