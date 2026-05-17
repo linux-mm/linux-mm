c98fa4566197cb58b7762d8a16ac0d7facf65f00
Thorsten Blum <thorsten.blum@linux.dev>
2026-05-17 14:34:30 +0200

    mm: add bytes_to_page_end() helper
    Add bytes_to_page_end() for the common PAGE_SIZE - offset_in_page()
    calculation.
    
    Suggested-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>

--- include/linux/page_helpers.h
+++ include/linux/page_helpers.h
@@ -6,5 +6,6 @@
 #include <asm/page.h>
 
 #define offset_in_page(p)	((unsigned long)(p) & ~PAGE_MASK)
+#define bytes_to_page_end(p)	(PAGE_SIZE - offset_in_page(p))
 
 #endif /* _LINUX_PAGE_HELPERS_H */
