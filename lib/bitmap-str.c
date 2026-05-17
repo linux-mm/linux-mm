c1ef2b6b0336dd80a046bbd32c02f975d9875928
Thorsten Blum <thorsten.blum@linux.dev>
2026-05-17 14:34:31 +0200

    lib/bitmap: use bytes_to_page_end() helper
    bitmap-str.c includes linux/mm.h for offset_in_page() and kfree().
    Instead, include linux/page_helpers.h and linux/slab.h directly, and
    use bytes_to_page_end() to simplify the code.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

--- lib/bitmap-str.c
+++ lib/bitmap-str.c
@@ -7,7 +7,8 @@
 #include <linux/export.h>
 #include <linux/hex.h>
 #include <linux/kernel.h>
-#include <linux/mm.h>
+#include <linux/page_helpers.h>
+#include <linux/slab.h>
 #include <linux/string.h>
 
 #include "kstrtox.h"
@@ -58,7 +59,7 @@ EXPORT_SYMBOL(bitmap_parse_user);
 int bitmap_print_to_pagebuf(bool list, char *buf, const unsigned long *maskp,
 			    int nmaskbits)
 {
-	ptrdiff_t len = PAGE_SIZE - offset_in_page(buf);
+	ptrdiff_t len = bytes_to_page_end(buf);
 
 	return list ? scnprintf(buf, len, "%*pbl\n", nmaskbits, maskp) :
 		      scnprintf(buf, len, "%*pb\n", nmaskbits, maskp);
