25a109fb98d683075d464918d0d72b6685fe88e8
Asier Gutierrez <gutierrez.asier@huawei-partners.com>
2026-05-22 14:55:15 +0000

    mm/damon: Generalize ctx_target creation for damon_ops_id and add vaddr support
    This patch adds a new function damon_modules_new_vaddr_ctx_target.
    Since ctx_target creation for vaddr and paddr is almost identical, the
    logic is extracted to a new function, damon_modules_new_ctx_target,
    and vaddr and paddr functions are left just as interfaces.
    
    This change was suggested earlier1 and it is needed to allow
    developers to create DAMON modules that use DAMON_OPS_VADDR targets.
    
    Signed-off-by: Asier Gutierrez <gutierrez.asier@huawei-partners.com>
    Suggested-by: SeongJae Park <sj@kernel.org>

--- mm/damon/modules-common.c
+++ mm/damon/modules-common.c
@@ -9,13 +9,8 @@
 
 #include "modules-common.h"
 
-/*
- * Allocate, set, and return a DAMON context for the physical address space.
- * @ctxp:	Pointer to save the point to the newly created context
- * @targetp:	Pointer to save the point to the newly created target
- */
-int damon_modules_new_paddr_ctx_target(struct damon_ctx **ctxp,
-		struct damon_target **targetp)
+static int _damon_modules_new_ctx_target(struct damon_ctx **ctxp,
+		struct damon_target **targetp, enum damon_ops_id id)
 {
 	struct damon_ctx *ctx;
 	struct damon_target *target;
@@ -24,7 +19,7 @@ int damon_modules_new_paddr_ctx_target(struct damon_ctx **ctxp,
 	if (!ctx)
 		return -ENOMEM;
 
-	if (damon_select_ops(ctx, DAMON_OPS_PADDR)) {
+	if (damon_select_ops(ctx, id)) {
 		damon_destroy_ctx(ctx);
 		return -EINVAL;
 	}
@@ -39,4 +34,26 @@ int damon_modules_new_paddr_ctx_target(struct damon_ctx **ctxp,
 	*ctxp = ctx;
 	*targetp = target;
 	return 0;
+}
+
+/*
+ * Allocate, set, and return a DAMON context for the physical address space.
+ * @ctxp:	Pointer to save the point to the newly created context
+ * @targetp:	Pointer to save the point to the newly created target
+ */
+int damon_modules_new_paddr_ctx_target(struct damon_ctx **ctxp,
+		struct damon_target **targetp)
+{
+	return _damon_modules_new_ctx_target(ctxp, targetp, DAMON_OPS_PADDR);
+}
+
+/*
+ * Allocate, set, and return a DAMON context for the virtual address space.
+ * @ctxp:	Pointer to save the point to the newly created context
+ * @targetp:	Pointer to save the point to the newly created target
+ */
+int damon_modules_new_vaddr_ctx_target(struct damon_ctx **ctxp,
+		struct damon_target **targetp)
+{
+	return _damon_modules_new_ctx_target(ctxp, targetp, DAMON_OPS_VADDR);
 }
