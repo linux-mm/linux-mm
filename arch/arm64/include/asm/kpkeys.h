f7e01348ab80a2479782baf73d6339ff4dfbc954
Kevin Brodsky <kevin.brodsky@arm.com>
2026-05-26 12:16:06 +0100

    arm64: kpkeys: Support KPKEYS_CTX_PGTABLES
    Enable RW access to KPKEYS_PKEY_PGTABLES (used to map page table
    pages) if switching to KPKEYS_CTX_PGTABLES, otherwise only grant RO
    access.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- arch/arm64/include/asm/kpkeys.h
+++ arch/arm64/include/asm/kpkeys.h
@@ -12,7 +12,8 @@
  * Equivalent to por_set_kpkeys_context(0, KPKEYS_CTX_DEFAULT), but can also be
  * used in assembly.
  */
-#define POR_EL1_INIT	POR_ELx_PERM_PREP(KPKEYS_PKEY_DEFAULT, POE_RWX)
+#define POR_EL1_INIT	(POR_ELx_PERM_PREP(KPKEYS_PKEY_DEFAULT, POE_RWX) | \
+			 POR_ELx_PERM_PREP(KPKEYS_PKEY_PGTABLES, POE_R))
 
 #ifndef __ASSEMBLY__
 
@@ -33,6 +34,8 @@ static inline bool arch_supports_kpkeys_early(void)
 static inline u64 por_set_kpkeys_context(u64 por, int ctx)
 {
 	por = por_elx_set_pkey_perms(por, KPKEYS_PKEY_DEFAULT, POE_RWX);
+	por = por_elx_set_pkey_perms(por, KPKEYS_PKEY_PGTABLES,
+				     ctx == KPKEYS_CTX_PGTABLES ? POE_RW : POE_R);
 
 	return por;
 }
