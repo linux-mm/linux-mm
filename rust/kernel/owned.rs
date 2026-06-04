b83ac7b53c96d5c41827e9413332338ba70c80c0
Andreas Hindborg <a.hindborg@kernel.org>
2026-06-04 22:11:15 +0200

    rust: implement `ForeignOwnable` for `Owned`
    Implement `ForeignOwnable` for `Owned<T>`. This allows use of `Owned<T>` in
    places such as the `XArray`.
    
    Note that `T` does not need to implement `ForeignOwnable` for `Owned<T>` to
    implement `ForeignOwnable`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/owned.rs
+++ rust/kernel/owned.rs
@@ -15,6 +15,8 @@ use core::{
     ptr::NonNull, //
 };
 
+use kernel::types::ForeignOwnable;
+
 /// Types that specify their own way of performing allocation and destruction. Typically, this trait
 /// is implemented on types from the C side.
 ///
@@ -108,6 +110,7 @@ pub trait Ownable {
 ///
 /// - Until `T::release` is called, this `Owned<T>` exclusively owns the underlying `T`.
 /// - The `T` value is pinned.
+#[repr(transparent)]
 pub struct Owned<T: Ownable> {
     ptr: NonNull<T>,
 }
@@ -183,5 +186,48 @@ impl<T: Ownable> Drop for Owned<T> {
         // SAFETY: By existence of `&mut self` we exclusively own `self` and the underlying `T`. As
         // we are dropping `self`, we can transfer ownership of the `T` to the `release` method.
         unsafe { T::release(self.ptr.as_mut()) };
+    }
+}
+
+// SAFETY: We derive the pointer to `T` from a valid `T`, so the returned
+// pointer satisfy alignment requirements of `T`.
+unsafe impl<T: Ownable + 'static> ForeignOwnable for Owned<T> {
+    const FOREIGN_ALIGN: usize = core::mem::align_of::<Owned<T>>();
+
+    type Borrowed<'a> = &'a T;
+    type BorrowedMut<'a> = Pin<&'a mut T>;
+
+    #[inline]
+    fn into_foreign(self) -> *mut kernel::ffi::c_void {
+        let ptr = self.ptr.as_ptr().cast();
+        core::mem::forget(self);
+        ptr
+    }
+
+    #[inline]
+    unsafe fn from_foreign(ptr: *mut kernel::ffi::c_void) -> Self {
+        Self {
+            // SAFETY: By function safety contract, `ptr` came from
+            // `into_foreign` and cannot be null.
+            ptr: unsafe { NonNull::new_unchecked(ptr.cast()) },
+        }
+    }
+
+    #[inline]
+    unsafe fn borrow<'a>(ptr: *mut kernel::ffi::c_void) -> Self::Borrowed<'a> {
+        // SAFETY: By function safety requirements, `ptr` is valid for use as a
+        // reference for `'a`.
+        unsafe { &*ptr.cast() }
+    }
+
+    #[inline]
+    unsafe fn borrow_mut<'a>(ptr: *mut kernel::ffi::c_void) -> Self::BorrowedMut<'a> {
+        // SAFETY: By function safety requirements, `ptr` is valid for use as a
+        // unique reference for `'a`.
+        let inner = unsafe { &mut *ptr.cast() };
+
+        // SAFETY: We never move out of inner, and we do not hand out mutable
+        // references when `T: !Unpin`.
+        unsafe { Pin::new_unchecked(inner) }
     }
 }
