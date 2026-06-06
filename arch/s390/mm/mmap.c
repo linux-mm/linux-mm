bd3028f85eb06a40624c01c5c8bfa5719245dc69
Oscar Salvador <osalvador@suse.de>
2026-06-06 05:49:59 +0200

    arch/s390: Stop special-casing hugetlb mappings in arch_get_unmapped_area
    arch_get_unmapped_area* sets info.align_mask to make room for extra alignment,
    so that is added on top of the length we request in unmapped_area{_topdown}.
    hugetlb_get_unmapped_area() already adds this extra padding in the 'len'
    parameter, and it also masks off the address it gets to properly align it to
    the huge_page_size we are using.
    
    So, stop special-casing hugetlb in arch_get_unmapped_area* functions.
    
    Also, there is no need to worry about align_offset because that will be
    masked off back in hugetlb_get_unmapped_area().
    
    Signed-off-by: Oscar Salvador <osalvador@suse.de>

--- arch/s390/mm/mmap.c
+++ arch/s390/mm/mmap.c
@@ -16,7 +16,6 @@
 #include <linux/sched/mm.h>
 #include <linux/random.h>
 #include <linux/security.h>
-#include <linux/hugetlb.h>
 #include <asm/elf.h>
 
 static unsigned long stack_maxrandom_size(void)
@@ -66,8 +65,6 @@ static inline unsigned long mmap_base(unsigned long rnd,
 
 static int get_align_mask(struct file *filp, unsigned long flags)
 {
-	if (filp && is_file_hugepages(filp))
-		return huge_page_mask_align(filp);
 	if (!(current->flags & PF_RANDOMIZE))
 		return 0;
 	if (filp || (flags & MAP_SHARED))
@@ -101,8 +98,7 @@ unsigned long arch_get_unmapped_area(struct file *filp, unsigned long addr,
 	info.low_limit = mm->mmap_base;
 	info.high_limit = TASK_SIZE;
 	info.align_mask = get_align_mask(filp, flags);
-	if (!(filp && is_file_hugepages(filp)))
-		info.align_offset = pgoff << PAGE_SHIFT;
+	info.align_offset = pgoff << PAGE_SHIFT;
 	addr = vm_unmapped_area(&info);
 	if (offset_in_page(addr))
 		return addr;
@@ -140,8 +136,7 @@ unsigned long arch_get_unmapped_area_topdown(struct file *filp, unsigned long ad
 	info.low_limit = PAGE_SIZE;
 	info.high_limit = mm->mmap_base;
 	info.align_mask = get_align_mask(filp, flags);
-	if (!(filp && is_file_hugepages(filp)))
-		info.align_offset = pgoff << PAGE_SHIFT;
+	info.align_offset = pgoff << PAGE_SHIFT;
 	addr = vm_unmapped_area(&info);
 
 	/*
