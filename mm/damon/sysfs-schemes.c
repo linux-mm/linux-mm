cb7f6af9d9f7a52765dacaa8048c534607e70085
Asier Gutierrez <gutierrez.asier@huawei-partners.com>
2026-06-11 15:02:43 +0000

    mm/damon/sysfs: support hugepage_mem_bp quota goal metric
    Add the "hugepage_mem_bp" to the sysfs-schemes interface.  Also handle
    DAMOS_QUOTA_HUGEPAGE_MEM_BP when constructing quota goals.
    
    Signed-off-by: Asier Gutierrez <gutierrez.asier@huawei-partners.com>

--- mm/damon/sysfs-schemes.c
+++ mm/damon/sysfs-schemes.c
@@ -1267,6 +1267,10 @@ struct damos_sysfs_qgoal_metric_name damos_sysfs_qgoal_metric_names[] = {
 		.metric = DAMOS_QUOTA_NODE_ELIGIBLE_MEM_BP,
 		.name = "node_eligible_mem_bp",
 	},
+	{
+		.metric = DAMOS_QUOTA_HUGEPAGE_MEM_BP,
+		.name = "hugepage_mem_bp",
+	},
 };
 
 static ssize_t target_metric_show(struct kobject *kobj,
@@ -2876,6 +2880,9 @@ static int damos_sysfs_add_quota_score(
 			break;
 		case DAMOS_QUOTA_NODE_ELIGIBLE_MEM_BP:
 			goal->nid = sysfs_goal->nid;
+			break;
+		case DAMOS_QUOTA_HUGEPAGE_MEM_BP:
+			goal->current_value = sysfs_goal->current_value;
 			break;
 		default:
 			break;
