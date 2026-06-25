316c06317712218590b6e5b61994581e78dfc374
Dev Jain <dev.jain@arm.com>
2026-06-25 11:29:51 +0000

    mm/rmap: use huge_ptep_get() in try_to_migrate_one()
    try_to_migrate_one() is used by folio migration to replace a present
    mapping with a migration entry. For hugetlb folios, page_vma_mapped_walk()
    returns the pte pointer to the hugetlb folio in pvmw.pte, but the code
    reads the huge pte entry with ptep_get().
    
    On arches which provide their own huge_ptep_get() to dereference a huge
    pte pointer, accessing via ptep_get() would cause pte_pfn(), pte_present()
    etc to misbehave.
    
    It is not clear whether this has a trivially visible effect to userspace.
    
    Use huge_ptep_get() to dereference a huge pte pointer.
    
    Commit a98a2f0c8ce1 copied the bug from try_to_unmap_one into
    try_to_migrate_one.
    
    Fixes: a98a2f0c8ce1 ("mm/rmap: split migration into its own function")
    Cc: stable@vger.kernel.org
    Signed-off-by: Dev Jain <dev.jain@arm.com>
    Acked-by: Muchun Song <muchun.song@linux.dev>

--- mm/rmap.c
+++ mm/rmap.c
@@ -2500,11 +2500,16 @@ static bool try_to_migrate_one(struct folio *folio, struct vm_area_struct *vma,
 		/* Unexpected PMD-mapped THP? */
 		VM_BUG_ON_FOLIO(!pvmw.pte, folio);
 
-		/*
-		 * Handle PFN swap PTEs, such as device-exclusive ones, that
-		 * actually map pages.
-		 */
-		pteval = ptep_get(pvmw.pte);
+		address = pvmw.address;
+		if (folio_test_hugetlb(folio)) {
+			pteval = huge_ptep_get(mm, address, pvmw.pte);
+		} else {
+			/*
+			 * Handle PFN swap PTEs, such as device-exclusive ones,
+			 * that actually map pages.
+			 */
+			pteval = ptep_get(pvmw.pte);
+		}
 		if (likely(pte_present(pteval))) {
 			pfn = pte_pfn(pteval);
 		} else {
@@ -2515,7 +2520,6 @@ static bool try_to_migrate_one(struct folio *folio, struct vm_area_struct *vma,
 		}
 
 		subpage = folio_page(folio, pfn - folio_pfn(folio));
-		address = pvmw.address;
 		anon_exclusive = folio_test_anon(folio) &&
 				 PageAnonExclusive(subpage);
 
