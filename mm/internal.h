30c7318fc20a3a63d12979c1c6f6f6fc10eac855
Lorenzo Stoakes <ljs@kernel.org>
2026-06-29 13:23:34 +0100

    mm/vma: make vma_set_range() static, drop insert_vm_struct() decl
    With __install_special_mapping() moved to vma.c, vma_set_range() can be
    made into a static function there and is now completely isolated from the
    rest of mm.
    
    While we're here, we can also remove the insert_vm_struct() declaration
    from mm.h - the function is implemented in vma.c and already declared in
    vma.h, and has no users outside of mm.
    
    Also update the VMA userland tests to reflect this change.
    
    No functional change intended.
    
    Signed-off-by: Lorenzo Stoakes <ljs@kernel.org>
    Reviewed-by: Gregory Price <gourry@gourry.net>

--- mm/internal.h
+++ mm/internal.h
@@ -1540,15 +1540,6 @@ extern bool mirrored_kernelcore;
 bool memblock_has_mirror(void);
 void memblock_free_all(void);
 
-static __always_inline void vma_set_range(struct vm_area_struct *vma,
-					  unsigned long start, unsigned long end,
-					  pgoff_t pgoff)
-{
-	vma->vm_start = start;
-	vma->vm_end = end;
-	vma->vm_pgoff = pgoff;
-}
-
 static inline bool vma_soft_dirty_enabled(struct vm_area_struct *vma)
 {
 	/*
