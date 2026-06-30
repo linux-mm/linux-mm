26d54620d1b990709dbab9b2664d0ea96c7cc4c4
Mike Rapoport (Microsoft) <rppt@kernel.org>
2026-06-30 13:52:31 +0300

    IB/mthca: mthca_reg_user_mr(): use kmalloc() to allocate addresses array
    mthca_reg_user_mr() allocates an array of DMA addresses during memory
    registration.
    
    This buffer can be allocated with kmalloc() as there's nothing special
    about it to go directly to the page allocator.
    
    kmalloc() provides a better API that does not require ugly casts and
    kfree() does not need to know the size of the freed object.
    
    Performance difference between kmalloc() and __get_free_pages() is not
    measurable as both allocators take an object/page from a per-CPU list for
    fast path allocations.
    
    For the slow path the performance is anyway determined by the amount of
    reclaim involved rather than by what allocator is used.
    
    Replace use of __get_free_page() with kmalloc() and free_page() with
    kfree().
    
    Link: https://lore.kernel.org/all/635405e4-9423-4a25-a6e7-e03c8ea0bcbe@redhat.com
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- drivers/infiniband/hw/mthca/mthca_provider.c
+++ drivers/infiniband/hw/mthca/mthca_provider.c
@@ -895,7 +895,7 @@ static struct ib_mr *mthca_reg_user_mr(struct ib_pd *pd, u64 start, u64 length,
 		goto err_umem;
 	}
 
-	pages = (u64 *) __get_free_page(GFP_KERNEL);
+	pages = kmalloc(PAGE_SIZE, GFP_KERNEL);
 	if (!pages) {
 		err = -ENOMEM;
 		goto err_mtt;
@@ -924,7 +924,7 @@ static struct ib_mr *mthca_reg_user_mr(struct ib_pd *pd, u64 start, u64 length,
 	if (i)
 		err = mthca_write_mtt(dev, mr->mtt, n, pages, i);
 mtt_done:
-	free_page((unsigned long) pages);
+	kfree(pages);
 	if (err)
 		goto err_mtt;
 
