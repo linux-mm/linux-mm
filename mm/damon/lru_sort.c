257d5df8bd861103c6925f119797df0aa680214c
SJ Park <sj@kernel.org>
2026-07-06 07:06:21 -0700

    mm/damon/lru_sort: ignore damon_stop() return value
    damon_stop() return value is guaranteed to be 0.  Ignore it.
    
    Link: https://lore.kernel.org/20260706140628.87414-7-sj@kernel.org
    Signed-off-by: SJ Park <sj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/damon/lru_sort.c
+++ mm/damon/lru_sort.c
@@ -410,8 +410,10 @@ static int damon_lru_sort_turn(bool on)
 {
 	int err;
 
-	if (!on)
-		return damon_stop(&ctx, 1);
+	if (!on) {
+		damon_stop(&ctx, 1);
+		return 0;
+	}
 
 	err = damon_lru_sort_apply_parameters();
 	if (err)
