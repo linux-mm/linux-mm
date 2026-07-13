3111c20acc002dbc83124e929b56a87c9e7bdff1
Yeoreum Yun <yeoreum.yun@arm.com>
2026-07-13 14:56:01 +0100

    x86: mm: use proper set_pXd() for generic compile-time folded patable in one_md_table_init()
    We want to rework how set_pXd() behaves for generic compile-time folded
    page tables by disallowing its use and triggering a compile-time error
    when it is used improperly, ensuring that the actual first-level
    set_pXd() function is used instead.
    
    Therefore, replace set_pgd() with set_pud() to setup first-level pgtable
    in case of X86_PAE.
    
    There should be no functional change.
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -70,19 +70,17 @@ static pmd_t * __init one_md_table_init(pgd_t *pgd)
 	pud_t *pud;
 	pmd_t *pmd_table;
 
+	p4d = p4d_offset(pgd, 0);
+	pud = pud_offset(p4d, 0);
 #ifdef CONFIG_X86_PAE
-	if (!(pgd_val(*pgd) & _PAGE_PRESENT)) {
+	if (!(pud_val(*pud) & _PAGE_PRESENT)) {
 		pmd_table = (pmd_t *)alloc_low_page();
-		set_pgd(pgd, __pgd(__pa(pmd_table) | _PAGE_PRESENT));
-		p4d = p4d_offset(pgd, 0);
-		pud = pud_offset(p4d, 0);
+		set_pud(pud, __pud(__pa(pmd_table) | _PAGE_PRESENT));
 		BUG_ON(pmd_table != pmd_offset(pud, 0));
 
 		return pmd_table;
 	}
 #endif
-	p4d = p4d_offset(pgd, 0);
-	pud = pud_offset(p4d, 0);
 	pmd_table = pmd_offset(pud, 0);
 
 	return pmd_table;
