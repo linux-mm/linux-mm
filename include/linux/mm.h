da4f6a6edf2439a64c34e7bd90dfc9de42976b4c
Kevin Brodsky <kevin.brodsky@arm.com>
2026-07-14 15:04:02 +0100

    mm: account page table pages when allocated
    As a step towards removing pagetable ctor/dtor calls for kernel
    mm's, move the memory accounting from the ctor/dtor to
    pagetable_{alloc,free}.
    
    The ctor is normally called after pagetable_alloc() (and dtor before
    pagetable_free()) so this patch should have no functional effect in
    most cases. Where calls to ctor/dtor are currently missing (e.g.
    x86's split_large_page()), this patch should improve the accounting.
    
    This patch is based on Matthew's earlier implementation linked
    below, with the difference that when freeing the accounting is
    updated in __pagetable_free(), meaning that the update is delayed
    until the pages are actually freed in the
    CONFIG_ASYNC_KERNEL_PGTABLE_FREE case. This shouldn't matter in
    practice.
    
    Suggested-by: "Matthew Wilcox (Oracle)" <willy@infradead.org>
    Link: https://lore.kernel.org/r/20251113140448.1814860-3-willy@infradead.org
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3743,6 +3743,11 @@ static inline struct ptdesc *pagetable_alloc_noprof(gfp_t gfp, unsigned int orde
 {
 	struct page *page = alloc_pages_noprof(gfp | __GFP_COMP, order);
 
+	if (!page)
+		return NULL;
+
+	lruvec_stat_add_folio(page_folio(page), NR_PAGETABLE);
+
 	return page_ptdesc(page);
 }
 #define pagetable_alloc(...)	alloc_hooks(pagetable_alloc_noprof(__VA_ARGS__))
@@ -3750,6 +3755,8 @@ static inline struct ptdesc *pagetable_alloc_noprof(gfp_t gfp, unsigned int orde
 static inline void __pagetable_free(struct ptdesc *pt)
 {
 	struct page *page = ptdesc_page(pt);
+
+	lruvec_stat_sub_folio(page_folio(page), NR_PAGETABLE);
 
 	__free_pages(page, compound_order(page));
 }
@@ -3859,7 +3866,6 @@ static inline void __pagetable_ctor(struct ptdesc *ptdesc)
 	struct folio *folio = ptdesc_folio(ptdesc);
 
 	__folio_set_pgtable(folio);
-	lruvec_stat_add_folio(folio, NR_PAGETABLE);
 }
 
 static inline void pagetable_dtor(struct ptdesc *ptdesc)
@@ -3868,7 +3874,6 @@ static inline void pagetable_dtor(struct ptdesc *ptdesc)
 
 	ptlock_free(ptdesc);
 	__folio_clear_pgtable(folio);
-	lruvec_stat_sub_folio(folio, NR_PAGETABLE);
 }
 
 static inline void pagetable_dtor_free(struct ptdesc *ptdesc)
