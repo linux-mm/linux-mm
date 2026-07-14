8dcfb431e86acdc95d4e8c7b203c27b279d7a085
Kevin Brodsky <kevin.brodsky@arm.com>
2026-07-14 15:04:03 +0100

    mm: set page table page type when allocated
    As a step towards removing pagetable ctor/dtor calls for kernel
    mm's, move the page type updates from the ctor/dtor to
    pagetable_{alloc,free}.
    
    __pagetable_ctor() is now empty so remove it;
    pagetable_{pud,p4d,pgd}_ctor() will be removed at a later stage.
    
    The ctor is normally called after pagetable_alloc() (and dtor before
    pagetable_free()) so this patch should have no functional effect in
    most cases. Where calls to ctor/dtor are currently missing (e.g.
    x86's split_large_page()), this patch will ensure the page type is
    set correctly.
    
    This patch is based on Matthew's earlier implementation linked
    below, with the difference that when freeing the page type is
    cleared in __pagetable_free(), meaning that the update is delayed
    until the pages are actually freed in the
    CONFIG_ASYNC_KERNEL_PGTABLE_FREE case. This shouldn't matter in
    practice.
    
    Suggested-by: "Matthew Wilcox (Oracle)" <willy@infradead.org>
    Link: https://lore.kernel.org/r/20251113140448.1814860-4-willy@infradead.org
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3761,6 +3761,7 @@ static inline struct ptdesc *pagetable_alloc_noprof(gfp_t gfp, unsigned int orde
 	if (!page)
 		return NULL;
 
+	__folio_set_pgtable(page_folio(page));
 	lruvec_stat_add_folio(page_folio(page), NR_PAGETABLE);
 
 	return page_ptdesc(page);
@@ -3771,6 +3772,7 @@ static inline void __pagetable_free(struct ptdesc *pt)
 {
 	struct page *page = ptdesc_page(pt);
 
+	__folio_clear_pgtable(page_folio(page));
 	lruvec_stat_sub_folio(page_folio(page), NR_PAGETABLE);
 
 	__free_pages(page, compound_order(page));
@@ -3876,19 +3878,9 @@ static inline bool ptlock_init(struct ptdesc *ptdesc) { return true; }
 static inline void ptlock_free(struct ptdesc *ptdesc) {}
 #endif /* defined(CONFIG_SPLIT_PTE_PTLOCKS) */
 
-static inline void __pagetable_ctor(struct ptdesc *ptdesc)
-{
-	struct folio *folio = ptdesc_folio(ptdesc);
-
-	__folio_set_pgtable(folio);
-}
-
 static inline void pagetable_dtor(struct ptdesc *ptdesc)
 {
-	struct folio *folio = ptdesc_folio(ptdesc);
-
 	ptlock_free(ptdesc);
-	__folio_clear_pgtable(folio);
 }
 
 static inline void pagetable_dtor_free(struct ptdesc *ptdesc)
@@ -3902,7 +3894,6 @@ static inline bool pagetable_pte_ctor(struct mm_struct *mm,
 {
 	if (!mm_is_kernel(mm) && !ptlock_init(ptdesc))
 		return false;
-	__pagetable_ctor(ptdesc);
 	return true;
 }
 
@@ -3994,7 +3985,6 @@ static inline bool pagetable_pmd_ctor(struct mm_struct *mm,
 	if (!mm_is_kernel(mm) && !pmd_ptlock_init(ptdesc))
 		return false;
 	ptdesc_pmd_pts_init(ptdesc);
-	__pagetable_ctor(ptdesc);
 	return true;
 }
 
@@ -4019,17 +4009,14 @@ static inline spinlock_t *pud_lock(struct mm_struct *mm, pud_t *pud)
 
 static inline void pagetable_pud_ctor(struct ptdesc *ptdesc)
 {
-	__pagetable_ctor(ptdesc);
 }
 
 static inline void pagetable_p4d_ctor(struct ptdesc *ptdesc)
 {
-	__pagetable_ctor(ptdesc);
 }
 
 static inline void pagetable_pgd_ctor(struct ptdesc *ptdesc)
 {
-	__pagetable_ctor(ptdesc);
 }
 
 extern void __init pagecache_init(void);
