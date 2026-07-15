6333fa490c6ceea6589bbdbffbfdff03cdad4a4f
Alexander Gordeev <agordeev@linux.ibm.com>
2026-07-15 15:22:36 +0200

    s390/mm: Batch PTE updates in lazy MMU mode
    Make use of the IPTE instruction's "Additional Entries" field to
    invalidate multiple PTEs in one go while in lazy MMU mode. This
    is the mode in which many memory-management system calls (like
    mremap(), mprotect(), etc.) update memory attributes.
    
    To achieve that, the set_pte() and ptep_get() primitives use a
    per-CPU cache to store and retrieve PTE values and apply the
    cached values to the real page table once lazy MMU mode is left.
    
    The same is done for memory-management platform callbacks that
    would otherwise cause intense per-PTE IPTE traffic, reducing the
    number of IPTE instructions from up to PTRS_PER_PTE to a single
    instruction in the best case. The average reduction is of course
    smaller.
    
    Since all existing page table iterators called in lazy MMU mode
    handle one table at a time, the per-CPU cache does not need to be
    larger than PTRS_PER_PTE entries. That also naturally aligns with
    the IPTE instruction, which must not cross a page table boundary.
    
    Before this change, the system calls did:
    
    	lazy_mmu_mode_enable_pte()
    	...
    	<update PTEs>		// up to PTRS_PER_PTE single-IPTEs
    	...
    	lazy_mmu_mode_disable()
    
    With this change, the system calls do:
    
        lazy_mmu_mode_enable_pte()
        ...
        <store new PTE values in the per-CPU cache>
        ...
        lazy_mmu_mode_disable()	// apply cache with one multi-IPTE
    
    When applied to large memory ranges, some system calls show
    significant speedups:
    
        mprotect()    ~15x
        munmap()      ~3x
        mremap()      ~28x
    
    The overall results depend on memory size and access patterns,
    but the change generally does not degrade performance.
    
    In addition to a process-wide impact, the rework affects the
    whole Central Electronics Complex (CEC). Each (global) IPTE
    instruction initiates a quiesce state in a CEC, so reducing
    the number of IPTE calls relieves CEC-wide quiesce traffic.
    
    In an extreme case of mprotect() contiguously triggering the
    quiesce state on four LPARs in parallel, measurements show
    ~25x fewer quiesce events.
    
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>

--- arch/s390/include/asm/lowcore.h
+++ arch/s390/include/asm/lowcore.h
@@ -163,7 +163,7 @@ struct lowcore {
 	__s32	preempt_count;			/* 0x03a8 */
 	__u32	spinlock_lockval;		/* 0x03ac */
 	__u32	spinlock_index;			/* 0x03b0 */
-	__u8	pad_0x03b4[0x03b8-0x03b4];	/* 0x03b4 */
+	__s32	lazy_mmu_count;			/* 0x03b4 */
 	__u64	percpu_offset;			/* 0x03b8 */
 	__u8	percpu_register;		/* 0x03c0 */
 	__u8	pad_0x03c1[0x0400-0x03c1];	/* 0x03c1 */
