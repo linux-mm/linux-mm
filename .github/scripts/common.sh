8cdaf002ec54d07950a52b6bb2d86ca927e43ad0
Kernel Patches Daemon <noreply@example.org>
2026-07-15 06:00:23 +0000

    adding ci files

---
+++ .github/scripts/common.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euo pipefail
+
+# Kselftest framework requirement - SKIP code is 4.
+ksft_skip=4
+
+test_script=$(basename $(realpath $0))
+linux_dir=${LINUX_DIR:-$(dirname $(realpath $0))"/../.."}
+mm_ci_dir=$(dirname $(realpath $0))"/../.."
+
+tmp_dir=$(mktemp -d)
+log=$tmp_dir/$test_script.log
+
+function cleanup() {
+    rm -fr "$tmp_dir"
+}
+trap cleanup EXIT
+
+function fail() {
+	local msg=${1:-"✗ Test failed"}
+
+	cat $log
+	echo "✗ $msg"
+	exit 1
+}
+
+function pass() {
+	local msg=${1:-"✗ Test passed"}
+
+	echo "✓ $msg"
+	exit 0
+}
