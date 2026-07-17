5117877384e183920d7822b99fcb375abd0b231c
Alexander Gordeev <agordeev@linux.ibm.com>
2026-07-17 15:32:19 +0200

    mm: use proper PTE accessors in madvise() and mremap()
    Follow the pattern established by commit c33c794828f2 ("mm:
    ptep_get() conversion") and use proper accessors instead of
    direct pointer dereferences.
    
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>

--- mm/madvise.c
+++ mm/madvise.c
@@ -1118,7 +1118,7 @@ static int guard_install_set_pte(unsigned long addr, unsigned long next,
 	unsigned long *nr_pages = (unsigned long *)walk->private;
 
 	/* Simply install a PTE marker, this causes segfault on access. */
-	*ptep = make_pte_marker(PTE_MARKER_GUARD);
+	set_pte(ptep, make_pte_marker(PTE_MARKER_GUARD));
 	(*nr_pages)++;
 
 	return 0;
