4e96dcb2d35c391a518ba5231f2a69322fd6dc58
Shiju Jose <shijujose2008@gmail.com>
2026-07-26 22:28:12 +0100

    ras: mem: Add ACPI RAS2 memory driver
    ACPI 6.5 Specification, section 5.2.21, defined RAS2 feature table (RAS2).
    Driver adds support for RAS2 feature table, which provides interfaces for
    platform RAS features, e.g., for HW-based memory scrubbing, and logical to
    PA translation service. RAS2 uses PCC channel subspace for communicating
    with the ACPI compliant HW platform.
    
    ACPI RAS2 auxiliary driver for the memory features binds to the auxiliary
    device, which is added by the RAS2 table parser in the ACPI RAS2 driver.
    
    Given the address range provided to the userspace may be the lowest scrub
    address range in the presence of disjoint address ranges, skipping address
    ranges that are from other NUMA nodes but happen to lie within this range.
    
    Driver retrieves the PA range of the NUMA domain and use it as the
    'Requested Address Range', when send RAS2 command GET_PATROL_PARAMETERS
    to get parameters that apply to all addresses in the NUMA domain.
    
    Device with ACPI RAS2 scrub feature registers with EDAC device driver,
    which retrieves the scrub descriptor from EDAC scrub and exposes
    the scrub control attributes for RAS2 scrub instance to userspace in
    /sys/bus/edac/devices/acpi_ras_memX/scrub0/.
    
    Add 'enable_demand' attribute to the EDAC scrub interface to start/stop
    the demand scrub, which is used in the RAS2 demand scrub control.
    When a demand scrub is started, any background scrub currently in progress
    will be stopped and then automatically restarted at the beginning when the
    demand scrub has completed.
    
    Co-developed-by: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    Tested-by: Daniel Ferguson <danielf@os.amperecomputing.com>
    Signed-off-by: Shiju Jose <shijujose2008@gmail.com>

--- include/acpi/ras2.h
+++ include/acpi/ras2.h
@@ -37,6 +37,22 @@ struct device;
  *			is associated with. See ACPI spec 6.5 Table 5.80: RAS2
  *			Platform Communication Channel Descriptor format,
  *			Field: Instance
+ * @mem_base:		Base of the lowest physical continuous memory range
+ *			of the memory associated with the NUMA domain
+ * @mem_size		Size of the lowest physical continuous memory range
+ *			of the memory associated with the NUMA domain
+ * @base:		Base address of the memory region to scrub
+ * @size:		Size of the memory region to scrub
+ * @scrub_cycle_hrs:	Current scrub rate in hours
+ * @set_scrub_cycle:	Scrub rate to set in hours
+ * @min_scrub_cycle:	Minimum scrub rate supported
+ * @max_scrub_cycle:	Maximum scrub rate supported
+ * @od_scrub:		Status of demand scrubbing (memory region)
+ * @bg_scrub:		Status of background patrol scrubbing
+ * @reenable_bg_scrub:	Flag indicates restart background scrubbing after demand
+ *			scrubbing is finished
+ * @thread:		Demand scrub monitor kthread
+ * @driver_active:	Flag indicates RAS2 memory driver is active/removed
  */
 struct ras2_mem_ctx {
 	struct auxiliary_device		adev;
@@ -45,6 +61,19 @@ struct ras2_mem_ctx {
 	void				*sspcc;
 	struct mutex			*pcc_lock;
 	u32				sys_comp_nid;
+	u64				mem_base;
+	u64				mem_size;
+	u64				base;
+	u64				size;
+	u8				scrub_cycle_hrs;
+	u8				set_scrub_cycle;
+	u8				min_scrub_cycle;
+	u8				max_scrub_cycle;
+	bool				od_scrub;
+	bool				bg_scrub;
+	bool				reenable_bg_scrub;
+	struct task_struct		*thread;
+	bool				driver_active;
 };
 
 #ifdef CONFIG_ACPI_RAS2
