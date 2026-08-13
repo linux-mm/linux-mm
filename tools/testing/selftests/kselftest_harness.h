87969ed3a3e9a6a94662134867b9f86789f6e48c
Hajime Tazaki <thehajime@gmail.com>
2026-08-13 15:34:00 +0900

    selftests: run tests on nommu architecture
    Architectures lacks MMU doesn't support fork(2) syscall and only
    vfork(2) is available with limitations.  Thus, we cannot run kselftest
    on nommu architecture as is.
    
    This commit addresses this issue with the following changes:
    
    - on build stage, add -DCONFIG_NOMMU to CFLAGS when NOMMU=1 variable
      added to the build/make argument.
    - on test run stage, avoid calling timeout command when NOMMU=1 variable
      added to environmental variable, since timeout command uses fork
      syscall which nommu platform doesn't support.
    - kselftest_harness.h warns if the file is include when building for
      NOMMU platform, as there is no fork(2) syscall.
    - replace "cd -" use as it is not available a shell supported on nommu
      (e.g., busybox hush), use cd "$OLDDIR" instead.
    - describe the difference of nommu tests in the document.
    
    So command line to build/execute tests for nommu should be like below:
    
     $ make ARCH=um NOMMU=1 O=build kselftest
     $ make ARCH=um NOMMU=1 -C tools/testing/selftests/mm run_tests
     $ NOMMU=1 /tmp/kselftest_install/run_kselftest.sh -s -c mm
    
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Kees Cook <kees@kernel.org>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Will Drewry <wad@chromium.org>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: Hangbin Liu <liuhangbin@gmail.com>
    Cc: "Ricardo B. Marliere" <rbm@suse.com>
    Cc: linux-kselftest@vger.kernel.org
    Cc: linux-um@lists.infradead.org
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- tools/testing/selftests/kselftest_harness.h
+++ tools/testing/selftests/kselftest_harness.h
@@ -1274,6 +1274,10 @@ static int test_harness_run(int argc, char **argv)
 	unsigned int count = 0;
 	unsigned int pass_count = 0;
 
+#ifdef CONFIG_NOMMU
+	ksft_print_msg("harness test doesn't support on NOMMU architecture (no fork(2)).\n");
+	return KSFT_SKIP;
+#endif /* CONFIG_NOMMU */
 	ret = test_harness_argv_check(argc, argv);
 	if (ret != KSFT_PASS)
 		return ret;
