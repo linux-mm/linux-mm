e8ae302c7efe15f108118fbf78ed892da774164b
Kevin Brodsky <kevin.brodsky@arm.com>
2026-08-18 15:08:55 +0100

    mm: kpkeys: Introduce early page table allocator
    The kpkeys_hardened_pgtables feature aims to protect all page table
    pages (PTPs) by mapping them with a privileged pkey. This is primarily
    handled by kpkeys_pgtable_alloc(), called from pagetable_alloc().
    However, this does not cover PTPs allocated early, before the
    buddy allocator is available. These PTPs are allocated by architecture
    code, either 1. from static pools or 2. using the memblock allocator,
    and should also be protected.
    
    This patch addresses the second category: PTPs allocated via memblock.
    Such PTPs are notably used to create the linear map. Protecting them as
    soon as they are allocated would require modifying the linear map while
    it is being created, which seems at best difficult. Instead, a
    simple allocator is introduced, obtaining pages from memblock and
    keeping track of all allocated ranges to set their pkey once it is
    safe to do so. PTPs allocated at that stage are not freed, so there
    is no need to manage a free list.
    
    Since kpkeys_hardened_pgtables currently requires the linear map to
    be PTE-mapped, we can directly allocate page by page using memblock,
    without intermediate cache. We rely on memblock allocating
    contiguous pages to minimise the number of tracked ranges.
    
    The number of PTPs required to create the linear map is proportional to
    the amount of available memory, which means it may be large. At such
    an early point, the memblock allocator may however only track a
    limited number of regions, and we size the tracking array
    (allocated_ranges) accordingly. The array may be quite large as a
    result (16KB on arm64), but it is discarded once boot has completed.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- mm/kpkeys_hardened_pgtables.c
+++ mm/kpkeys_hardened_pgtables.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include <linux/kpkeys.h>
+#include <linux/memblock.h>
 #include <linux/mm.h>
 #include <linux/set_memory.h>
 
@@ -29,6 +30,9 @@ static int set_pkey_default(struct page *page, unsigned int nr_pages)
 	WARN_ON(ret);
 	return ret;
 }
+
+/* pkeys physmem allocator (PPA) - implemented below */
+static void ppa_finalize(void);
 
 struct page *kpkeys_pgtable_alloc(gfp_t gfp, unsigned int order)
 {
@@ -60,4 +64,115 @@ void __init kpkeys_hardened_pgtables_init(void)
 		return;
 
 	static_branch_enable(&kpkeys_hardened_pgtables_key);
+
+	ppa_finalize();
+}
+
+/*
+ * pkeys physmem allocator (PPA): allocator for very early page tables
+ * (especially for creating the linear map), based on memblock. Allocated
+ * ranges are tracked so that their pkey can be set once it is safe to do so.
+ */
+
+/*
+ * We may have to track many ranges when allocating page tables for the linear
+ * map, as their number grows with the amount of available memory. Assuming that
+ * memblock returns contiguous blocks whenever possible, the number of ranges
+ * to track cannot however exceed the number of regions that memblock itself
+ * tracks. memblock_allow_resize() hasn't been called yet at that point, so
+ * that limit is the size of the statically allocated array.
+ */
+#define PHYSMEM_MAX_RANGES	INIT_MEMBLOCK_MEMORY_REGIONS
+
+struct physmem_range {
+	phys_addr_t addr;
+	phys_addr_t size;
+};
+
+struct pkeys_physmem_allocator {
+	struct physmem_range allocated_ranges[PHYSMEM_MAX_RANGES];
+	unsigned int nr_allocated_ranges;
+};
+
+static struct pkeys_physmem_allocator pkeys_physmem_allocator __initdata;
+
+static int __init set_pkey_pgtable_phys(phys_addr_t pa, phys_addr_t size)
+{
+	unsigned long addr = (unsigned long)__va(pa);
+	int ret;
+
+	ret = set_memory_pkey(addr, size / PAGE_SIZE, KPKEYS_PKEY_PGTABLES);
+
+	WARN_ON(ret);
+	return ret;
+}
+
+static bool __init ppa_try_extend_last_range(phys_addr_t addr, phys_addr_t size)
+{
+	struct pkeys_physmem_allocator *ppa = &pkeys_physmem_allocator;
+	struct physmem_range *range;
+
+	if (!ppa->nr_allocated_ranges)
+		return false;
+
+	range = &ppa->allocated_ranges[ppa->nr_allocated_ranges - 1];
+
+	/* Merge the new range into the last range if they are contiguous */
+	if (addr == range->addr + range->size) {
+		range->size += size;
+		return true;
+	} else if (addr + size == range->addr) {
+		range->addr -= size;
+		range->size += size;
+		return true;
+	}
+
+	return false;
+}
+
+static void __init ppa_register_allocated_range(phys_addr_t addr,
+						phys_addr_t size)
+{
+	struct pkeys_physmem_allocator *ppa = &pkeys_physmem_allocator;
+	struct physmem_range *range;
+
+	if (!addr)
+		return;
+
+	if (ppa_try_extend_last_range(addr, size))
+		return;
+
+	/* Could not extend the last range, create a new one */
+	if (WARN_ON(ppa->nr_allocated_ranges >= PHYSMEM_MAX_RANGES))
+		return;
+
+	range = &ppa->allocated_ranges[ppa->nr_allocated_ranges++];
+	range->addr = addr;
+	range->size = size;
+}
+
+static void __init ppa_finalize(void)
+{
+	struct pkeys_physmem_allocator *ppa = &pkeys_physmem_allocator;
+
+	for (unsigned int i = 0; i < ppa->nr_allocated_ranges; i++) {
+		struct physmem_range *range = &ppa->allocated_ranges[i];
+
+		set_pkey_pgtable_phys(range->addr, range->size);
+	}
+}
+
+phys_addr_t __ref kpkeys_physmem_pgtable_alloc(void)
+{
+	size_t size = PAGE_SIZE;
+	phys_addr_t addr;
+
+	addr = memblock_phys_alloc_range(size, size, 0,
+					 MEMBLOCK_ALLOC_NOLEAKTRACE);
+	if (!addr)
+		return addr;
+
+	ppa_register_allocated_range(addr, size);
+
+	return addr;
 }
