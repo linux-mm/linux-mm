75f88c1455ce38e021fc50151f67d011d9643006
Fabio M. De Francesco <fabio.m.de.francesco@linux.intel.com>
2026-08-25 04:26:16 +0200

    cxl/pci: Make the HDM and Mem_Enable writes callable from CXL
    Split the Global Control write out of devm_cxl_enable_hdm() as
    cxl_enable_hdm() and drop the static from cxl_set_mem_enable(), so both
    are reachable from the rest of cxl_core. Declare them in core.h.
    
    Signed-off-by: Fabio M. De Francesco <fabio.m.de.francesco@linux.intel.com>

--- drivers/cxl/core/core.h
+++ drivers/cxl/core/core.h
@@ -213,6 +213,8 @@ int cxl_gpf_port_setup(struct cxl_dport *dport);
 struct cxl_hdm;
 int cxl_hdm_decode_init(struct cxl_dev_state *cxlds, struct cxl_hdm *cxlhdm,
 			struct cxl_endpoint_dvsec_info *info);
+void cxl_enable_hdm(struct cxl_hdm *cxlhdm, u32 global_ctrl);
+int cxl_set_mem_enable(struct cxl_dev_state *cxlds, u16 val);
 int cxl_port_get_possible_dports(struct cxl_port *port);
 
 #ifdef CONFIG_CXL_FEATURES
