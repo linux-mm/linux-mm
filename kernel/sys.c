1d87123989f6bbd148e8c60ffc0742bcf8863a9d
André Almeida <andrealmeid@igalia.com>
2026-08-27 15:23:03 -0300

    sched: Extend task command name with TASK_COMM_EXT_LEN
    Command name has been restrict to only 16 bytes, which is too limiting,
    specially when debugging and tracing complex software with thousands of
    threads and the need to differentiate them.
    
    Just as it was done with kthreads in commit 6b59808bfe48 ("workqueue:
    Show the latest workqueue name in /proc/PID/{comm,stat,status}"), support
    long names for userspace threads as well.
    
    To avoid buffer overflows, cap all existing userspace APIs to
    TASK_COMM_LEN, and leave the full extended name for a new interface.
    
    Signed-off-by: André Almeida <andrealmeid@igalia.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2535,7 +2535,7 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		unsigned long, arg4, unsigned long, arg5)
 {
 	struct task_struct *me = current;
-	unsigned char comm[sizeof(me->comm)];
+	unsigned char comm[TASK_COMM_LEN];
 	long error;
 
 	error = security_task_prctl(option, arg2, arg3, arg4, arg5);
@@ -2601,16 +2601,16 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 			error = -EINVAL;
 		break;
 	case PR_SET_NAME:
-		comm[sizeof(me->comm) - 1] = 0;
+		comm[TASK_COMM_LEN - 1] = 0;
 		if (strncpy_from_user(comm, (char __user *)arg2,
-				      sizeof(me->comm) - 1) < 0)
+				      TASK_COMM_LEN - 1) < 0)
 			return -EFAULT;
 		set_task_comm(me, comm);
 		proc_comm_connector(me);
 		break;
 	case PR_GET_NAME:
-		strscpy_pad(comm, me->comm);
-		if (copy_to_user((char __user *)arg2, comm, sizeof(comm)))
+		strscpy_pad(comm, me->comm, TASK_COMM_LEN);
+		if (copy_to_user((char __user *)arg2, comm, TASK_COMM_LEN))
 			return -EFAULT;
 		break;
 	case PR_GET_ENDIAN:
