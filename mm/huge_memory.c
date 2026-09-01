57967ca26f46aa20087c4ff6714f26bea3d0dccc
Luiz Capitulino <luizcap@redhat.com>
2026-08-31 23:12:18 -0400

    mm: thp: always enable mTHP support
    If PMD-sized pages are not supported on an architecture (ie. the
    arch implements arch_has_pmd_leaves() and it returns false) then the
    current code disables all THP, including mTHP.
    
    This commit fixes this by allowing mTHP to be always enabled for all
    archs. When PMD-sized pages are not supported, its sysfs entry won't be
    created and their mapping will be disallowed at page-fault time.
    
    Similarly, this commit implements the following changes for shmem in
    shmem_allowable_huge_orders():
    
     - Drop the pgtable_has_pmd_leaves() check so that mTHP sizes are
       considered
     - Filter out PMD and PUD orders from allowable orders when
       PMD-sized pages are not supported by the CPU
    
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Signed-off-by: Luiz Capitulino <luizcap@redhat.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -197,6 +197,15 @@ unsigned long __thp_vma_allowable_orders(struct vm_area_struct *vma,
 	else
 		supported_orders = THP_ORDERS_ALL_FILE_DEFAULT;
 
+	if (!pgtable_has_pmd_leaves()) {
+		/*
+		 * If the CPU does not support PMD leaves, assume for
+		 * now that it does not support PUD leaves and disable
+		 * both folio orders.
+		 */
+		supported_orders &= ~(BIT(PMD_ORDER) | BIT(PUD_ORDER));
+	}
+
 	orders &= supported_orders;
 	if (!orders)
 		return 0;
@@ -204,7 +213,7 @@ unsigned long __thp_vma_allowable_orders(struct vm_area_struct *vma,
 	if (!vma->vm_mm)		/* vdso */
 		return 0;
 
-	if (!pgtable_has_pmd_leaves() || vma_thp_disabled(vma, vm_flags, forced_collapse))
+	if (vma_thp_disabled(vma, vm_flags, forced_collapse))
 		return 0;
 
 	/* khugepaged doesn't collapse DAX vma, but page fault is fine. */
@@ -984,7 +993,7 @@ static int __init hugepage_init_sysfs(struct kobject **hugepage_kobj)
 	 * disable all other sizes. powerpc's PMD_ORDER isn't a compile-time
 	 * constant so we have to do this here.
 	 */
-	if (!anon_orders_configured)
+	if (!anon_orders_configured && pgtable_has_pmd_leaves())
 		huge_anon_orders_inherit = BIT(PMD_ORDER);
 
 	*hugepage_kobj = kobject_create_and_add("transparent_hugepage", mm_kobj);
@@ -1006,6 +1015,15 @@ static int __init hugepage_init_sysfs(struct kobject **hugepage_kobj)
 	}
 
 	orders = THP_ORDERS_ALL_ANON | THP_ORDERS_ALL_FILE_DEFAULT;
+	if (!pgtable_has_pmd_leaves()) {
+		/*
+		 * If the CPU does not support PMD leaves, assume for
+		 * now that it does not support PUD leaves and disable
+		 * both folio orders.
+		 */
+		orders &= ~(BIT(PMD_ORDER) | BIT(PUD_ORDER));
+	}
+
 	order = highest_order(orders);
 	while (orders) {
 		thpsize = thpsize_create(order, *hugepage_kobj);
@@ -1094,9 +1112,6 @@ static int __init hugepage_init(void)
 {
 	int err;
 	struct kobject *hugepage_kobj;
-
-	if (!pgtable_has_pmd_leaves())
-		return -EINVAL;
 
 	/*
 	 * hugepages can't be allocated by the buddy allocator
