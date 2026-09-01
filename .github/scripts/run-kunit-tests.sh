46dd8e49cecbc123576a0ce82c2e95562a1fb6b8
Kernel Patches Daemon <noreply@example.org>
2026-09-01 05:43:55 +0000

    adding ci files

---
+++ .github/scripts/run-kunit-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+source "$(dirname "$0")/common.sh"
+
+cd $linux_dir
+
+echo "Running SLUB KUnit test"
+vng --cpus 4 --memory 8G --numa 4G --numa 4G \
+    -- bash -c 'modprobe slub_kunit; dmesg' &> $log || fail "Failed to run test"
+
+if grep -qE 'not ok [0-9]+ slub_test\b' $log; then
+	fail "SLUB KUnit test failed"
+elif grep -qE '\bok [0-9]+ slub_test\b' $log; then
+	pass "SLUB KUnit test passed"
+else
+	fail "SLUB KUnit test did not run"
+fi
