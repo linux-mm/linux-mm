3404f5338e15dc25869c72b0d1ce2ece7913f532
David Hildenbrand (Arm) <david@kernel.org>
2026-09-02 12:56:21 +0100

    mm/pgtable: disallow calling (pgd|p4d|pud)_page, pgd_page_vaddr() and (p4d|pud)_pgtable with dummy
    These helpers silently fallback to the folded helpers. With e.g., pdgp_get()
    returning dummy values, pgd_page_vaddr(pgdp_get(pgdp)) will now behave
    differently than pgd_page_vaddr(*pgdp).
    
    Disallow calling (pgd|p4d|pud)_page() entirely, and catch when passing a
    dummy value to pgd_page_vaddr() and (p4d|pud)_pgtable.
    This will catch most abuse of dummy pXdp value at compile time --
    so far there were none, and it better stay that way.
    
    Signed-off-by: David Hildenbrand (Arm) <david@kernel.org>
    Co-developed-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- include/asm-generic/pgtable-nopud.h
+++ include/asm-generic/pgtable-nopud.h
@@ -77,8 +77,12 @@ static __always_inline pud_t *__pud_offset_lockless(p4d_t *p4dp, p4d_t p4d,
 #define pud_val(x)				(p4d_val((x).p4d))
 #define __pud(x)				((pud_t) { __p4d(x) })
 
-#define p4d_page(p4d)				(pud_page((pud_t){ p4d }))
-#define p4d_pgtable(p4d)			((pud_t *)(pud_pgtable((pud_t){ p4d })))
+#define p4d_page(p4d)				({ BUILD_BUG(); (struct page *)NULL; })
+#define p4d_pgtable(p4d)						\
+({									\
+	p4d_check_dummy(p4d);						\
+	((pud_t *)(pud_pgtable((pud_t) { p4d })));			\
+})
 
 /*
  * allocating and freeing a pud is trivial: the 1-entry pud is
