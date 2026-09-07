debd2ba646192c14f44f38ff1147cf7892a505d9
Jinjie Ruan <ruanjinjie@huawei.com>
2026-09-07 20:54:04 +0800

    arm64: crash: Add crash hotplug support
    When CPU or memory hotplug events occur, the elfcorehdr in the kdump
    image becomes stale, potentially leading to incomplete crash dumps.
    
    Currently, userspace udev rules reload the entire kdump image upon such
    events, which is inefficient and leaves kdump inactive for a long time.
    
    Commit 247262756121 ("crash: add generic infrastructure for crash hotplug
    support") introduced a kernel mechanism to update only the elfcorehdr.
    This patch enables that support for arm64.
    
    On arm64, only memory hotplug events require elfcorehdr updates:
    - Physical CPU hotplug is not supported.
    
    - For ACPI based vCPU hotplug [1], the elfcorehdr is built using
      for_each_possible_cpu(), so no update is needed.
    
    The patch:
    - Adds CONFIG_ARCH_SUPPORTS_CRASH_HOTPLUG (default y).
    
    - Implements following arch functions to handle memory hotplug:
      1. arch_crash_hotplug_support()
      2. arch_crash_get_elfcorehdr_size()
      3. arch_crash_handle_hotplug_event()
    
    - Moves arch_get_system_nr_ranges() and arch_crash_populate_cmem()
      from machine_kexec_file.c to crash.c for crash hotplug reuse.
    
    Follows the approach of x86 commit ea53ad9cf73b ("x86/crash: add x86 crash
    hotplug support") and powerpc commit b741092d5976 ("powerpc/crash: add
    crash CPU hotplug support").
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: "Mike Rapoport (Microsoft)" <rppt@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Breno Leitao <leitao@debian.org>
    Cc: Sourabh Jain <sourabhjain@linux.ibm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Thomas Huth <thuth@redhat.com>
    [1]: https://lore.kernel.org/all/20240529133446.28446-1-Jonathan.Cameron@huawei.com/
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/machine_kexec_file.c
+++ arch/arm64/kernel/machine_kexec_file.c
@@ -14,7 +14,6 @@
 #include <linux/kernel.h>
 #include <linux/kexec.h>
 #include <linux/libfdt.h>
-#include <linux/memblock.h>
 #include <linux/of.h>
 #include <linux/of_fdt.h>
 #include <linux/slab.h>
@@ -39,34 +38,6 @@ int arch_kimage_file_post_load_cleanup(struct kimage *image)
 	return kexec_image_post_load_cleanup_default(image);
 }
 
-#ifdef CONFIG_CRASH_DUMP
-unsigned int arch_get_system_nr_ranges(void)
-{
-	unsigned int nr_ranges = 2 + crashk_cma_cnt; /* for exclusion of crashkernel region */
-	phys_addr_t start, end;
-	u64 i;
-
-	for_each_mem_range(i, &start, &end)
-		nr_ranges++;
-
-	return nr_ranges;
-}
-
-int arch_crash_populate_cmem(struct crash_mem *cmem)
-{
-	phys_addr_t start, end;
-	u64 i;
-
-	for_each_mem_range(i, &start, &end) {
-		cmem->ranges[cmem->nr_ranges].start = start;
-		cmem->ranges[cmem->nr_ranges].end = end - 1;
-		cmem->nr_ranges++;
-	}
-
-	return 0;
-}
-#endif
-
 /*
  * Tries to add the initrd and DTB to the image. If it is not possible to find
  * valid locations, this function will undo changes to the image and return non
@@ -89,8 +60,9 @@ int load_other_segments(struct kimage *image,
 	kbuf.buf_min = kernel_load_addr + kernel_size;
 
 #ifdef CONFIG_CRASH_DUMP
+	unsigned long nr_ranges = 0;
 	if (image->type == KEXEC_TYPE_CRASH) {
-		ret = crash_prepare_headers(true, &kbuf.buffer, &kbuf.bufsz, NULL);
+		ret = crash_prepare_headers(true, &kbuf.buffer, &kbuf.bufsz, &nr_ranges);
 		if (ret) {
 			pr_err("Preparing elf core header failed\n");
 			goto out_err;
@@ -100,10 +72,13 @@ int load_other_segments(struct kimage *image,
 			vfree(image->elf_headers);
 
 		image->elf_headers = kbuf.buffer;
-		image->elf_headers_sz = kbuf.bufsz;
 
 		kbuf.mem = KEXEC_BUF_MEM_UNKNOWN;
-		kbuf.memsz = kbuf.bufsz;
+		kbuf.memsz = kbuf.bufsz + crash_extra_elfcorehdr_size(nr_ranges);
+		image->elf_headers_sz = kbuf.memsz;
+#ifdef CONFIG_CRASH_HOTPLUG
+		image->elfcorehdr_index = image->nr_segments;
+#endif
 		kbuf.buf_align = SZ_64K; /* largest supported page size */
 		kbuf.buf_max = ULONG_MAX;
 		kbuf.top_down = true;
@@ -177,6 +152,9 @@ int load_other_segments(struct kimage *image,
 	return 0;
 
 out_err:
+#ifdef CONFIG_CRASH_HOTPLUG
+	image->elfcorehdr_index = -1;
+#endif
 	while (image->nr_segments > orig_segments) {
 		kexec_free_segment_cma(image, image->nr_segments - 1);
 		image->nr_segments--;
