b2dd27a1a2548b20ab6d1aa8487fb71edf8f8395
Lorenzo Stoakes (ARM) <ljs@kernel.org>
2026-09-08 21:01:36 +0100

    mm/madvise: use predicates for madvise(..., MADV_DOFORK)
    Make it clear what we're blocking in MADV_DOFORK. Previously we simply
    disallowed VM_SPECIAL i.e. kernel-owned mappings, fixed mappings and
    VMA_IO_BIT.
    
    Now the invariant is established that only kernel-owned mappings can set
    VMA_IO_BIT, the VMA_IO_BIT check is redundant.
    
    The rest is equivalent to testing for a kernel-owned or fixed mapping,
    i.e. exactly the same check as whether the VMA is permitted to be merged.
    
    This was established by commit 0b2758f48f22 ("Require (reasonably) normal
    mappings for MADV_DOFORK") containing my hands-down favourite call out of
    all time.
    
    Express the same thing differently - if we wouldn't be allowed to merge it,
    then we aren't allowed to manipulate CoW behaviour on fork.
    
    No functional change intended.
    
    Signed-off-by: Lorenzo Stoakes (ARM) <ljs@kernel.org>

--- mm/madvise.c
+++ mm/madvise.c
@@ -1401,7 +1401,7 @@ static int madvise_vma_behavior(struct madvise_behavior *madv_behavior)
 		new_flags |= VM_DONTCOPY;
 		break;
 	case MADV_DOFORK:
-		if (new_flags & VM_SPECIAL)
+		if (!vma_can_merge(vma))
 			return -EINVAL;
 		new_flags &= ~VM_DONTCOPY;
 		break;
