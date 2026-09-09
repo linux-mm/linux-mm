7cf27e1c7c8b13afc131f71288f678c83095e9aa
Hugh Dickins <hughd@google.com>
2026-09-09 03:14:11 -0700

    mm/fbatch: remove lru_cache_disable() from NUMA folio migration
    Remove lru_cache_disable() from mbind(MOVE), migrate_pages and move_pages
    syscall handling. They do not now benefit from lru_add_drain_all() first;
    they gain little benefit from invalidating buffer head LRUs first, since
    5.0 commit 80409c65e2c6 ("mm: migrate: make buffer_migrate_page_norefs()
    actually succeed"); and it's a shame that (even without CAP_SYS_NICE)
    they can prevent concurrent tasks from enjoying the use of those caches.
    
    Signed-off-by: Hugh Dickins <hughd@google.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2359,8 +2359,6 @@ static int do_pages_move(struct mm_struct *mm, nodemask_t task_nodes,
 	int start, i;
 	int err = 0, err1;
 
-	lru_cache_disable();
-
 	for (i = start = 0; i < nr_pages; i++) {
 		const void __user *p;
 		int node;
@@ -2439,7 +2437,6 @@ out_flush:
 	if (err >= 0)
 		err = err1;
 out:
-	lru_cache_enable();
 	return err;
 }
 
