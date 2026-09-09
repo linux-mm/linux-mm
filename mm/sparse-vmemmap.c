6178fc70288b9f0b88c2f5308a33685b7a08a874
David Hildenbrand (Arm) <david@kernel.org>
2026-09-09 15:33:02 +0200

    mm/sparse: remove SECTION_MARKED_PRESENT
    All present section iterators run before memory hotplug added any
    further memory sections, Therefore, we can simply use the SECTION_IS_EARLY
    flag by setting that flag earlier in sparse_prepare_early_sections().
    
    Get rid of SECTION_MARKED_PRESENT entirely and rename
    for_each_present_section_nr() to for_each_early_section_nr().
    
    Also update the gdb script to use the updated value for
    SECTION_IS_EARLY.
    
    No functional change intended.
    
    Signed-off-by: David Hildenbrand (Arm) <david@kernel.org>
    Acked-by: Oscar Salvador <osalvador@suse.de>
    Acked-by: Lorenzo Stoakes (ARM) <ljs@kernel.org>

--- mm/sparse-vmemmap.c
+++ mm/sparse-vmemmap.c
@@ -890,7 +890,6 @@ int __meminit sparse_add_section(int nid, unsigned long start_pfn,
 	page_init_poison(memmap, sizeof(struct page) * nr_pages);
 
 	ms = __nr_to_section(section_nr);
-	__section_mark_present(ms, section_nr);
 
 	/* Align memmap to section boundary in the subsection case */
 	if (section_nr_to_pfn(section_nr) != start_pfn)
