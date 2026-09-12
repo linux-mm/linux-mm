6fc8910217fb8262ef07a73545f796766f101e19
André Almeida <andrealmeid@igalia.com>
2026-09-11 22:23:58 -0300

    treewide: Get rid of get_task_comm()
    Since commit 4cc0473d7754 ("get rid of __get_task_comm()"),
    get_task_comm() does just a redundant check for the buffer size and call
    strscpy_pad(). Replace get_task_comm() calls with strscpy_pad(), that will
    do the right thing if the buffers sizes doesn't match: zero-pad if it's
    bigger, and truncate if it's smaller.
    
    Link: https://lore.kernel.org/lkml/CAHk-=wi5c=_-FBGo_88CowJd_F-Gi6Ud9d=TALm65ReN7YjrMw@mail.gmail.com/
    Signed-off-by: André Almeida <andrealmeid@igalia.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_fence.c
@@ -73,7 +73,7 @@ struct amdgpu_amdkfd_fence *amdgpu_amdkfd_fence_create(u64 context,
 	/* This reference gets released in amdkfd_fence_release */
 	mmgrab(mm);
 	fence->mm = mm;
-	get_task_comm(fence->timeline_name, current);
+	strscpy_pad(fence->timeline_name, current->comm);
 	spin_lock_init(&fence->lock);
 	fence->context_id = context_id;
 	dma_fence_init(&fence->base, &amdkfd_fence_ops, &fence->lock,
