cfc0972e0abb279a6e4798c40119ad594356c839
Christian Brauner <brauner@kernel.org>
2026-09-15 13:31:03 +0200

    um: commit fds on syscall exit
    Add TIF_FD_SLOTS. handle_syscall() calls syscall_trace_leave() after
    every system call, so test the flag there and commit the reservations
    before audit and the ptrace exit stop.
    
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- arch/um/include/asm/thread_info.h
+++ arch/um/include/asm/thread_info.h
@@ -44,6 +44,7 @@ struct thread_info {
 #define TIF_SECCOMP		9	/* secure computing */
 #define TIF_SINGLESTEP		10	/* single stepping userspace */
 #define TIF_SYSCALL_TRACEPOINT	11	/* syscall tracepoint instrumentation */
+#define TIF_FD_SLOTS		12	/* syscall prepared descriptors */
 
 
 #define _TIF_SYSCALL_TRACE	(1 << TIF_SYSCALL_TRACE)
@@ -55,6 +56,7 @@ struct thread_info {
 #define _TIF_NOTIFY_RESUME	(1 << TIF_NOTIFY_RESUME)
 #define _TIF_SECCOMP		(1 << TIF_SECCOMP)
 #define _TIF_SINGLESTEP		(1 << TIF_SINGLESTEP)
+#define _TIF_FD_SLOTS		(1 << TIF_FD_SLOTS)
 
 #define _TIF_WORK_MASK		(_TIF_NEED_RESCHED | _TIF_SIGPENDING | _TIF_NOTIFY_SIGNAL | \
 				 _TIF_NOTIFY_RESUME)
